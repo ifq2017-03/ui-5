c2af7fd62feaa6b6c920b490b4cf6749e9373cab
itq2017-03 <ifq2017-03@cloud.bs-hassfurt.de>
2019-07-11 11:40:03 +0200

    super

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
-sap.ui.define([
+/*sap.ui.define([
+	"sap/m/Text",
+	"sap/ui/core/HTML",
+	"sap/m/Link"
+	
+], 
 
-], function () {
+function (Text, HTML, Link) {
+	"use strict";
+	new Text({
+		text: "Hello Faggot11111",
+		}).placeAt("content");
+	
+	new HTML({
+		content: "<br/><br/>",
+			}).placeAt("content");
+		
+
+	new Link({
+			text: "Hello Faggot",
+			href: "https://google.com"
+		}).placeAt("content");
+
+});*/
+
+sap.ui.define([
+	"sap/ui/core/mvc/XMLView"
+], function (XMLView) {
 	"use strict";
 
+	XMLView.create({
+		viewName: "sap.ui.demo.walkthrough.view.app"
+	}).then(function (oView) {
+		oView.placeAt("content");
+	});
 
-	alert("UI5 is ready");
 });
