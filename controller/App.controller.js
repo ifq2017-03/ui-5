1cac5b3e28ac3b6aa76aa4ae02b9d7342a4bffed
itq2017-03 <ifq2017-03@cloud.bs-hassfurt.de>
2019-07-12 10:27:50 +0200

    nuss

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,13 +1,16 @@
 sap.ui.define([
    "sap/ui/core/mvc/Controller",
-   "sap/m/MessageToast"
+   "sap/m/MessageToast",
    "sap/m/MessageBox"
-], function (Controller, MessageToast) {
+], function (Controller, MessageToast, MessageBox) {
    "use strict";
    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
       onShowHello : function () {
          // show a native JavaScript alert
          MessageToast.show("ALARM!!!!");
+      },
+      AlarmButtonPress : function () {
+         // show a native JavaScript alert
          MessageBox.alert("ALARM!!!!");
       }
    });  
