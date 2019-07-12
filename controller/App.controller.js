84b10a57c74a66e167ce930323d0cc34388c7c32
ifq2017-03 <52657464+ifq2017-03@users.noreply.github.com>
2019-07-12 08:31:15 +0200

    Update App.controller.js
    nana

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,8 +1,8 @@
 sap.ui.define([
    "sap/ui/core/mvc/Controller",
-   "sap/m/MessageToast"
+   "sap/m/MessageToast",
    "sap/m/MessageBox"
-], function (Controller, MessageToast) {
+], function (Controller, MessageToast, MessageBox) {
    "use strict";
    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
       onShowHello : function () {
