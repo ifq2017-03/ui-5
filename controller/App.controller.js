32249e1860f51ad28631359b597193dbdb74e486
itq2017-03 <ifq2017-03@cloud.bs-hassfurt.de>
2019-07-12 10:21:07 +0200

    fadfsdffs

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,12 +1,14 @@
 sap.ui.define([
    "sap/ui/core/mvc/Controller",
    "sap/m/MessageToast"
+   "sap/m/MessageBox"
 ], function (Controller, MessageToast) {
    "use strict";
    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
       onShowHello : function () {
          // show a native JavaScript alert
          MessageToast.show("ALARM!!!!");
+         MessageBox.alert("ALARM!!!!");
       }
    });  
 }); 
